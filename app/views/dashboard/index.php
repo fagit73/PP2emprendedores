4e3b4629da870b3f3ef4e5f2636144f92ad06bcd
Champi <dreynoso.adm@gmail.com>
2026-06-12 21:30:08 -0300

    se realiza maqueta dashboard

--- app/views/dashboard/index.php
+++ app/views/dashboard/index.php
@@ -3,6 +3,39 @@
 <div class="welcome">
     <h1>Bienvenido, <?php echo $_SESSION['nombre']; ?></h1>
     <p>Este es tu panel de administración (Dashboard).</p>
+
+
+    <ul>
+        <li>
+            <a href="#">
+                Inicio
+            </a>
+        </li>
+    </ul>
+
+    <ul>
+        <li>
+            <a href="#">
+                Mis reservas
+            </a>
+        </li>
+    </ul>
+
+    <ul>
+        <li>
+            <a href="#">
+                Inicio
+            </a>
+        </li>
+    </ul>
+
+    <ul>
+        <li>
+            <a href="#">
+                Inicio
+            </a>
+        </li>
+    </ul>
     
     <a href="<?php echo URLAPP; ?>/auth/logout">Cerrar Sesión</a>
 </div>
